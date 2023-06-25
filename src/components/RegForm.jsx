6959aabb79d30a6c8ea2a370c857ad372c1b8148
Nitin Chaudhary <nitin.io@outlook.com>
2023-06-25 06:37:59 +0530

    Fix: Checkbox not getting reset after submitting form

--- src/components/RegForm.jsx
+++ src/components/RegForm.jsx
@@ -1,9 +1,9 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field } from "formik";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { BsEyeSlash, BsEye } from "react-icons/bs";
 
-// Validation Function to pass in formik.
+// Validation Function to pass in
 const validate = (values) => {
   const errors = {};
 
@@ -34,126 +34,120 @@ const RegForm = () => {
   // Using for show and hide password by changing
   // input type password to text and vice versa
   const [checked, setChecked] = useState(false);
-  const [isSubmitting, setSubmitting] = useState(false);
 
-  const formik = useFormik({
-    // Initial Values
-    initialValues: {
-      fullname: "",
-      email: "",
-      password: "",
-      validByUser: false,
-    },
-    validate,
-    onSubmit: (values) => {
-      setSubmitting(true);
-      console.log(
-        `New user create with name ${values.fullname} and email ${values.email}. Valid: ${values.validByUser}`
-      ),
-        formik.resetForm();
-
-      toast.success("Form is submitted successfully");
-      setChecked(false);
-      setSubmitting(false);
-    },
-  });
+  // const formik = useFormik({
+  //   // Initial Values
+  //   ,
+  //   validate,
+  //   onSubmit: ,
+  // });
 
   return (
-    <form onSubmit={formik.handleSubmit} className="p-5 rounded-2">
-      <div className="mb-3">
-        <label htmlFor="fullname" className="form-label">
-          Full Name
-        </label>
-        <input
-          type="text"
-          name="fullname"
-          id="fullname"
-          disabled={isSubmitting}
-          placeholder="eg - Nitin Chaudhary"
-          value={formik.values.fullname}
-          onChange={formik.handleChange}
-          className="form-control"
-        />
-        {formik.errors.fullname && formik.touched.fullname ? (
-          <span className="form-text text-warning">
-            {formik.errors.fullname}
-          </span>
-        ) : null}
-      </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Email Address
-        </label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          disabled={isSubmitting}
-          placeholder="example@example.com"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          className="form-control"
-        />
-        {formik.errors.email && formik.touched.email ? (
-          <span className="form-text text-warning">{formik.errors.email}</span>
-        ) : null}
-      </div>
-      <div className="mb-3 password-container">
-        <label htmlFor="password" className="form-label">
-          Password
-        </label>
-        <span id="eye-icon">
-          {checked ? (
-            <BsEye onClick={() => setChecked(!checked)} />
-          ) : (
-            <BsEyeSlash onClick={() => setChecked(!checked)} />
-          )}
-        </span>
-        <input
-          type={checked ? "text" : "password"}
-          name="password"
-          id="password"
-          disabled={isSubmitting}
-          placeholder="Enter Password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          className="form-control"
-        />
-      </div>
-      {formik.errors.password && formik.touched.password ? (
-        <span className="form-text text-warning">{formik.errors.password}</span>
-      ) : null}
-      <div className="mb-3">
-        <br />
-        <input
-          type="checkbox"
-          name="validByUser"
-          disabled={isSubmitting}
-          value={formik.values.validByUser}
-          onChange={formik.handleChange}
-          className="form-check-input"
-        />
-        <label htmlFor="email" className="form-check-label">
-          &nbsp;I consent that all the details are true.
-        </label>
-        {formik.errors.validByUser && formik.touched.validByUser ? (
-          <span className="form-text text-warning">
+    <Formik
+      initialValues={{
+        fullname: "",
+        email: "",
+        password: "",
+        validByUser: false,
+      }}
+      validate={validate}
+      onSubmit={(values, { resetForm }) => {
+        console.log(
+          `New user create with name ${values.fullname} and email ${values.email}. Valid: ${values.validByUser}`
+        ),
+          toast.success("Form is submitted successfully");
+        resetForm();
+        setChecked(false);
+      }}
+    >
+      {({ errors, touched, isSubmitting }) => (
+        <Form className="p-5 rounded-2">
+          <div className="mb-3">
+            <label htmlFor="fullname" className="form-label">
+              Full Name
+            </label>
+            <Field
+              type="text"
+              name="fullname"
+              id="fullname"
+              disabled={isSubmitting}
+              placeholder="eg - Nitin Chaudhary"
+              className="form-control"
+            />
+            {errors.fullname && touched.fullname ? (
+              <span className="form-text text-warning">{errors.fullname}</span>
+            ) : null}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email Address
+            </label>
+            <Field
+              type="text"
+              name="email"
+              id="email"
+              disabled={isSubmitting}
+              placeholder="example@example.com"
+              className="form-control"
+            />
+            {errors.email && touched.email ? (
+              <span className="form-text text-warning">{errors.email}</span>
+            ) : null}
+          </div>
+          <div className="mb-3 password-container">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <span id="eye-icon">
+              {checked ? (
+                <BsEye onClick={() => setChecked(!checked)} />
+              ) : (
+                <BsEyeSlash onClick={() => setChecked(!checked)} />
+              )}
+            </span>
+            <Field
+              type={checked ? "text" : "password"}
+              name="password"
+              id="password"
+              disabled={isSubmitting}
+              placeholder="Enter Password"
+              className="form-control"
+            />
+          </div>
+          {errors.password && touched.password ? (
+            <span className="form-text text-warning">{errors.password}</span>
+          ) : null}
+          <div className="mb-3">
             <br />
-            {formik.errors.validByUser}
-          </span>
-        ) : null}
-      </div>
-      <div>
-        <button
-          type="submit"
-          disabled={isSubmitting}
-          className="btn btn-warning text-white"
-        >
-          Register
-        </button>
-      </div>
-      {/* Toast Container to show notification */}
-    </form>
+            <Field
+              type="checkbox"
+              name="validByUser"
+              disabled={isSubmitting}
+              className="form-check-input"
+            />
+            <label htmlFor="email" className="form-check-label">
+              &nbsp;I consent that all the details are true.
+            </label>
+            {errors.validByUser && touched.validByUser ? (
+              <span className="form-text text-warning">
+                <br />
+                {errors.validByUser}
+              </span>
+            ) : null}
+          </div>
+          <div>
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="btn btn-warning text-white"
+            >
+              Register
+            </button>
+          </div>
+          {/* Toast Container to show notification */}
+        </Form>
+      )}
+    </Formik>
   );
 };
 
